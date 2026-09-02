c8e6b96b3cd5b17cf2b91aee5a6e01490b98b8ae
Ali-Jaun57 <alijaun221@gmail.com>
2026-09-02 14:26:07 +0500

    Refactor comments in Valid_Anagram.py
    Updated comments for consistency and clarity.

--- 2. Valid_Anagram.py
+++ 2. Valid_Anagram.py
@@ -1,35 +1,35 @@
---------------------------------
-DESCRIPTION
---------------------------------
+# --------------------------------
+# DESCRIPTION
+# --------------------------------
 
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
- 
-
-Example 1:
-
-Input: s = "anagram", t = "nagaram"
-
-Output: true
-
-Example 2:
-
-Input: s = "rat", t = "car"
-
-Output: false
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
  
 
-Constraints:
+# Example 1:
 
-1 <= s.length, t.length <= 5 * 104
-s and t consist of lowercase English letters.
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "rat", t = "car"
+
+# Output: false
+
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
 
 
 
-----------------------------------
-CODE FOR TASK
-----------------------------------
+# ----------------------------------
+# CODE FOR TASK
+# ----------------------------------
 class Solution(object):
     def isAnagram(self, s, t):
         """
